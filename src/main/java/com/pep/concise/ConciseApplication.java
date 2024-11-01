09e7f001b360dab01ff6b58117648ae3ef430376
gang.liu <15093472310@163.com>
2024-11-01 16:49:54 +0800

    initial

---
+++ src/main/java/com/pep/concise/ConciseApplication.java
@@ -0,0 +1,22 @@
+package com.pep.concise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author liu.gang
+ */
+@SpringBootApplication
+public class ConciseApplication {
+
+    /**
+     * 启动方法
+     *
+     * @param args 启动参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ConciseApplication.class, args);
+    }
+}
